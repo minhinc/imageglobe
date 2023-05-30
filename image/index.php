cad2712d2f4ee74a2957f50dbf30eba9c9474c3f
Minh Inc <tominhinc@gmail.com>
2023-05-30 10:59:32 +0530

    initial version

---
+++ image/index.php
@@ -0,0 +1,66 @@
+<? require_once('./php/Utilm.php'); $util=new Utilc; ?>
+<html>
+<head>
+<title>Minh, Inc. Software development and Outsourcing | Home</title>
+<link rel="stylesheet" type="text/css" href="./css/main.css" media="all"/>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-81633607-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+<body>
+<?echo "hello"; $util->drawheader('main'); ?>
+<div class="leftpan">
+ <a href="./product/home.html"><img src="./image/mytda.png"/></a>
+ <div class="research">
+  <ul class="research">
+   <a href="./research/home.html"><li class="header"><p>Research</p></li></a>
+   <a href="http://www.codeguru.com/IoT/coding-analog-sensors-on-the-raspberry-pi3.html"><li class="light"><p class="t">Coding Analog Sensors On The Raspberry Pi3</p><p class="b">codeguru.com,Sep 2017</p><img src="./image/dv.png"/></li></a>
+   <a href="http://www.codeguru.com/IoT/coding-sensors-on-the-rpi3.html"><li class="light"><p class="t">Coding Sensors On The Raspberry Pi3</p><p class="b">codeguru.com,Sep 2017</p><img src="./image/dv.png"/></li></a>
+   <a href="http://www.codeguru.com/IoT/raspberry-pi-3-hardware-and-system-software-reference.html"><li class="light"><p class="t">Raspberry Pi 3 Hardware And System Software Reference</p><p class="b">codeguru.com,Sep 2017</p><img src="./image/dv.png"/></li></a>
+   <a href="http://www.codeguru.com/IoT/using-the-qt-2d-display-on-a-raspberry-pi3.html"><li class="light"><p class="t">The Qt 2d Display on a Raspberry pi3</p><p class="b">codeguru.com,Sep 2017</p><img src="./image/dv.png"/></li></a>
+   <a href="http://www.developer.com/open/accessing-files-using-python.html"><li class="dark"><p class="t">Accessing Files Using Python</p><p class="b">developer.com,Jan 2017</p><img src="./image/dev.png"/></li></a>
+   <a href="http://www.codeguru.com/cpp/g-m/drawing-3d-opengl-graphics-on-google-maps.html"><li class="light"><p class="t">Qt OpenGL 3D drawing on Google Maps</p><p class="b">codeguru.com,May 2016</p><img src="./image/dv.png"/></li></a>
+   <a href="http://www.codeguru.com/tools/commsoftfreecondit/qt-basics-the-chain-of-responsibility-pattern.html"><li class="dark"><p class="t">Qt Basic: The Chain of Responsiblity Pattern</p><p class="b">codeguru.com,Nov 2015</p><img src="./image/dv.png"/></li></a>
+   <a href="http://www.codeproject.com/Articles/869923/Class-Level-Generic-Logger"><li class="light"><p class="t">C++ Class Level Generic Logger</p><p class="b">codeproject.com,Jan 2015</p><img src="./image/cp.png"/></li></a>
+   <a href="./research/SDJ_Open_2014.pdf"><li class="dark"><p class="t">Design Patterns in Perl</p><p class="b">SDJ,Nov 2014</p><img src="./image/sdj.png"/></li></a>
+   <a href="http://www.ibm.com/developerworks/aix/library/au-aix-stack-tree-traversal"><li class="light"><p class="t">Stack Based Breadth First Tree Traversal</p><p class="b">IBM dw,Mar 2013</p><img src="./image/dw.png"/></li></a>
+  </ul>
+  </div>
+  <div class="product">
+  <ul class="research">
+   <a href="./product/home.html"><li class="header"><p>Product</p></li></a>
+   <a href="./product/nfs.html"><li class="light"><p class="t">Qt Based 3D OpenGL Flight Simulator on Google Maps</p><p class="b">Aug 2016</p><img src="./image/nz.png"/></li></a>
+   <a href="./product/gmp.html"><li class="dark"><p class="t">Qt Based Media Player.</p><p class="b">Aug 2016</p><img src="./image/gmp.png"/></li></a>
+   <a href="./product/gfd.html"><li class="light"><p class="t">Qt Based YouTube Downloader</p><p class="b">Aug 2016</p><img src="./image/gfd.png"/></li></a>
+   <a href="./product/pas.html"><li class="dark"><p class="t">Medical Annotation Software</p><p class="b red">Upcoming</p></li></a>
+   <a href="./product/dv.html"><li class="light"><p class="t">PythonGL based 3D data viewer</p><p class="b red">Upcoming</p></li></a>
+  </ul>
+  </div>
+</div>
+
+<div class="rightpan">
+ <ul class="events">
+  <li class="header"><p>Upcoming Events</p></li>
+  <li class="light"><p class="t">Annotation Tool</p><p class="b"></p></li>
+  <li class="dark"><p class="t">Building Qt on Raspberry Pi</p><p class="b"></p></li>
+  <li class="light"><p class="t">WayLand Window Manager support on Embedded device</p><p class="b"></p></li>
+  <li class="dark"><p class="t">Python based 3D openGL based graphics</p><p class="b"></p></li>
+ </ul>
+ <ul class="next">
+  <a href="./training.home.html"><li class="header"><p>Training</p></li></a>
+  <li class="light"><a class="l" href="./training/advance-py-agenda.pdf">Agenda</a><a href="./training/py.html"><img class="l" src="./image/python.png"/></a><a class="r" href="./training/advance-cpp-agenda.pdf">Agenda</a><a class="rr" href="./training/advance-cpp-slides.pdf">Slides</a><a href="./training/cpp.html"><img class="r" src="./image/cpp.png"/></a></li>
+  <li class="dark"><a class="ls" href="./training/advance-qt-agenda.html">Slides</a><a href="./training/qt.html"><img class="l" src="./image/qt.png"/></a><a class="r" href="./training/advance-c-agenda.pdf">Agenda</a><a class="rr" href="./training/advance-c-slides.pdf">Slides</a><a href="./training/c.html"><img class="r" src="./image/c.png"/></a></li>
+  <li class="light"><a class="ls" href="./training/advance-gl-agenda.html">Slides</a><a href="./training/opengl.html"><img class="l" src="./image/opengl.png"/></a><a class="r" href="./training/advance-ldd-agenda.pdf">Agenda</a><a href="./training/ldd.html"><img class="r" src="./image/ldd.png"/></a></li>
+  <li class="dark"><a class="l" href="./training/advance-li-agenda.pdf">Agenda</a><a class="lr" href="./training/advance-linux-slides.pdf">Slides</a><a href="./training/li.html"><img class="l" src="./image/li.png"/></a><a class="rs" href="./training/advance-qml-agenda.html">Slides</a><a href="./training/qt.html"><img class="r" src="./image/qml.png"/></a></li>
+ </ul>
+</div>
+<? $util->drawfooter('main','.',); ?>
+</body>
+</html>
